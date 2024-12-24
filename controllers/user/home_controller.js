6f6c5546a6ad4a3ed25cf16d1f7204dcbb257e9e
unknown <ahmadafif613@gmail.com>
2024-12-24 13:27:51 +0300

    a

--- controllers/user/home_controller.js
+++ controllers/user/home_controller.js
@@ -1,4 +1,6 @@
 const Banner = require("../../models/banner");
+const MealPlan = require("../../models/meals/meal_plan");
+const Package = require("../../models/package");
 
 exports.getBanner = async (req, res, next) => {
     try {
@@ -6,5 +8,31 @@ exports.getBanner = async (req, res, next) => {
         res.status(200).json(banners);
     } catch (error) {
         next(error);
+    }
+}
+exports.getHomePlans = async (req, res, next) => {
+    try {
+        const plans = await MealPlan.findAll({
+            limit: 5,
+            order: [
+                'createdAt', 'DESC'
+            ]
+        })
+        res.status(200).json(plans)
+    } catch (e) {
+        next(e)
+    }
+}
+exports.getHomePackages = async (req, res, next) => {
+    try {
+        const packages = await Package.findAll({
+            limit: 5,
+            order: [
+                'createdAt', 'DESC'
+            ]
+        })
+        res.status(200).json(plans)
+    } catch (e) {
+        next(e)
     }
 }
--- routes/home_route.js
+++ routes/home_route.js
@@ -2,4 +2,6 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/user/home_controller")
 router.get("/banner", controller.getBanner)
+router.get("/plans", controller.getHomePlans)
+router.get("/packages", controller.getHomePackages)
 module.exports = router
