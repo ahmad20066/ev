026452b0a44a40ff9a9d3b64921b2e64dbd972f5
unknown <ahmadafif613@gmail.com>
2025-01-15 11:39:43 +0300

    fixes

--- controllers/admin/admin_stats_controller.js
+++ controllers/admin/admin_stats_controller.js
@@ -35,7 +35,7 @@ exports.activeSubscriptionsFitness = async (req, res, next) => {
             ],
             group: [sequelize.fn('MONTH', sequelize.col('Subscription.createdAt'))],
             order: [[sequelize.fn('MONTH', sequelize.col('Subscription.createdAt')), 'ASC']],
-            raw: true
+            // raw: true
         });
 
         const subscriptionData = new Array(12).fill(0);
