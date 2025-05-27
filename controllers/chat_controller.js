e0f5b26a19f27de17b1e0ba7a08db1d8a15093ab
unknown <ahmadafif613@gmail.com>
2025-05-27 05:05:00 +0300

    aa

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -1,3 +1,4 @@
+const sequelize = require("../models");
 const Chat = require("../models/chat/chat");
 const ChatRequest = require("../models/chat/chat_request");
 const Message = require("../models/chat/message");
