52679516df72874b25a8539b976d0998ff7a14f9
unknown <ahmadafif613@gmail.com>
2025-06-22 15:25:27 +0300

    aa

--- models/fitness/coupon.js
+++ models/fitness/coupon.js
@@ -21,7 +21,7 @@ const Coupon = sequelize.define('Coupon', {
         allowNull: false
     },
     expiry_date: {
-        type: DataTypes.DATE,
+        type: DataTypes.DATEONLY,
         allowNull: false
     },
     usage_limit: {
