5a7876a62c97d22b1301108910a91e220e7f1a20
unknown <a.afif@tgmena.com>
2025-11-10 15:34:13 +0300

    aa

--- routes/fitness_route.js
+++ routes/fitness_route.js
@@ -55,5 +55,5 @@ router.get("/package-workouts", controller.getPackageWorkouts)
 router.get("/package-workouts/:id", controller.showPackageWorkout)
 router.post("/apply-coupon", controller.applyCouponToPackage)
 router.get("/exercises", controller.getExercises)
-router.get("/diary", hasSubscription, controller.getPerformanceStats)
+router.get("/diary", controller.getPerformanceStats)
 module.exports = router;
